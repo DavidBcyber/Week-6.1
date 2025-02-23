0e56bac1d7b950d81a477391f6aa941168322490
xbatm <xbatm@MSI>
2025-02-23 16:30:27 -0600

    Add project files.

---
+++ 6.1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace _6._1
+{
+    class MoblibePhone // prototype/ custom data type
+    {
+        // properties of the class /object
+        public string brand { get; set; }
+        public string model { get; set; }
+        public int storage { get; set; }
+        public int price { get; set; }
+
+        public MoblibePhone(string brand, string model, int storage, int price)
+        {
+            this.brand = brand;
+            this.model = model;
+            this.storage = storage;
+            this.price = price;
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($" {brand} -- {model} -- {storage} -- {price} ");
+
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MoblibePhone mobliephone = new MoblibePhone("Apple", "iPhone 10", 280, 1000);
+            mobliephone.DisplayInfo();
+
+
+            MoblibePhone mobilephone2 = new MoblibePhone("Samsung", "Fold3", 450, 899);
+            mobilephone2.DisplayInfo();
+
+
+        }
+
+    }
+}
